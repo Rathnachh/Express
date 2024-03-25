48d0c6f710bb1599d1ca56b4815db09618d63473
Rathnachh <rathna.chh@gmail.com>
2024-03-25 15:04:28 +0700

    add swagger

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,11 +2,14 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import { studentRouter } from "../routes/student.route";
 import { movieRouter } from "../routes/movie.route";
 import path from "path";
+import { swaggerDocument } from "../swagger";
+import swaggerUi from "swagger-ui-express";
 
 function createServer(): Express {
   const app = express();
   app.set("view engine", "ejs");
   app.set("views", path.join(__dirname, "/views"));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.use(express.json());
 
@@ -16,8 +19,8 @@ function createServer(): Express {
 
   app.use("/student", studentRouter);
   app.use("/movie", movieRouter);
-//   app.use(express.json());
-// app.use("/movies", movieRoutes);
+  //   app.use(express.json());
+  // app.use("/movies", movieRoutes);
 
   app.all(
     "*",
